637cca0bfbe36fcbaca1f27d36d3960104e57ddd
Elvan H <elvanhd@gmail.com>
2022-03-14 21:28:40 +0700

    controlling scroll behaviour

--- src/main.js
+++ src/main.js
@@ -43,6 +43,14 @@ const router = createRouter({
       component: NotFound,
     },
   ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
 });
 
 const app = createApp(App);
