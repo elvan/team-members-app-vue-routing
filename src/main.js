f5fb39729bd31b0d288fbca37d737c8a5d9737a8
Elvan H <elvanhd@gmail.com>
2022-03-14 21:20:09 +0700

    rendering multiple routes with named router views

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import NotFound from './components/nav/NotFound.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
 import TeamsList from './components/teams/TeamsList.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
@@ -15,7 +17,10 @@ const router = createRouter({
     },
     {
       path: '/teams',
-      component: TeamsList,
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter,
+      },
       // alias: '/',
       children: [
         {
@@ -28,7 +33,10 @@ const router = createRouter({
     },
     {
       path: '/users',
-      component: UsersList,
+      components: {
+        default: UsersList,
+        footer: UsersFooter,
+      },
     },
     {
       path: '/:notFound(.*)',
