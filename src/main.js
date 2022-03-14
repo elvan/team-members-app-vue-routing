61746cb01aa2cf9442a86b591534d1a05ebe2bc8
Elvan H <elvanhd@gmail.com>
2022-03-14 08:33:58 +0700

    routing setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: App,
+    },
+  ],
+});
+
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
