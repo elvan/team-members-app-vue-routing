4d5cfac557168b072822a4584ab9dc1e1ba292e8
Elvan Hidayat <elvanhd@gmail.com>
2024-03-05 21:00:45 +0700

    Add NotFound component and routing for 404 errors
    - Create NotFound.vue for displaying a message on unknown routes
    - Integrate NotFound component into routing with a catch-all path
    - Redirect root path '/' to '/teams' for default landing page
    - Ensure that all unmatched routes redirect to NotFound page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import NotFound from './components/nav/NotFound.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
@@ -8,9 +9,11 @@ import UsersList from './components/users/UsersList.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
