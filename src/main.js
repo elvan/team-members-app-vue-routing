691ff4adbcf1c47c4b1f66cc25ba7e3d36b7e098
Elvan Hidayat <elvanhd@gmail.com>
2024-03-05 21:24:28 +0700

    Implement named views for footer components
    - Add footer section in App.vue for displaying footer content
    - Create TeamsFooter.vue and UsersFooter.vue components for respective footers
    - Update router in main.js to use named views for displaying footers in TeamsList and UsersList
    - Configure TeamsList and UsersList routes to render specific footer components using named views

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import NotFound from './components/nav/NotFound.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
 import TeamsList from './components/teams/TeamsList.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
@@ -13,7 +15,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
           name: 'team-members',
@@ -23,7 +25,13 @@ const router = createRouter({
         }, // /teams/t1
       ],
     }, // our-domain.com/teams => TeamsList
-    { path: '/users', component: UsersList },
+    {
+      path: '/users',
+      components: {
+        default: UsersList,
+        footer: UsersFooter,
+      },
+    },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
