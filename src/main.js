be430a302db0624cc8c3a901aad8cb23d913649f
Elvan Hidayat <elvanhd@gmail.com>
2024-03-05 21:12:39 +0700

    Refactor team member routing and navigation
    - Modify TeamsItem.vue to use named route for link to team members
    - Add <router-view> in TeamsList.vue for nested routing of team members
    - Refactor routing in main.js to include team members as a nested route under teams
    - Use named routes for better readability and maintenance in navigation

--- src/main.js
+++ src/main.js
@@ -10,9 +10,20 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    {
+      name: 'teams',
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        }, // /teams/t1
+      ],
+    }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
