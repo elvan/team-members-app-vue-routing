9bde4098bbed8cb913629a664939ef3b402f6c80
Elvan Hidayat <elvanhd@gmail.com>
2024-03-06 07:47:19 +0700

    Refactor routing and organize components into pages
    - Move Vue Router setup to a separate file (src/router.js) for cleaner main.js
    - Rename and relocate component files to reflect a more intuitive structure, distinguishing between pages and components
    - Adjust imports in pages (TeamsList, UsersList) to correctly reference component files
    - Implement global beforeEach navigation guard in router.js to log and manage access based on route metadata
    - Use meta fields in route definitions to exemplify conditional navigation logic (e.g., authentication checks)

--- src/main.js
+++ src/main.js
@@ -1,66 +1,6 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-import UsersList from './components/users/UsersList.vue';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      components: { default: TeamsList, footer: TeamsFooter },
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true,
-        }, // /teams/t1
-      ],
-    }, // our-domain.com/teams => TeamsList
-    {
-      path: '/users',
-      components: {
-        default: UsersList,
-        footer: UsersFooter,
-      },
-      beforeEnter(to, from, next) {
-        console.log('users beforeEnter');
-        console.log(to, from);
-        next();
-      },
-    },
-    { path: '/:notFound(.*)', component: NotFound },
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(_, _2, savedPosition) {
-    // console.log(to, from, savedPosition);
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach(function (to, from, next) {
-  console.log('Global beforeEach');
-  console.log(to, from);
-
-  next();
-});
-
-router.afterEach(function (to, from) {
-  // sending analytics data
-  console.log('Global afterEach');
-  console.log(to, from);
-});
+import router from './router.js';
 
 const app = createApp(App);
 
